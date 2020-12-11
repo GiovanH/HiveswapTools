5d08a60f13bdad6711ae87ab19684b5d190422fb
Gio <sethg@ipi.org>
2020-12-10 20:48:07 -0600

    Graph stuff

--- UnityBehaviorExplorer.py
+++ UnityBehaviorExplorer.py
@@ -7,6 +7,7 @@ import collections
 import re
 from functools import lru_cache
 import textwrap
+import pickle
 
 try:
     from tqdm import tqdm
@@ -24,10 +25,6 @@ def safe(x):
 
 file_paths = sorted(glob.glob("*/MonoBehaviour/*"))
 
-referencesFrom = collections.defaultdict(list)
-referencedBy = collections.defaultdict(list)
-referencedAs = collections.defaultdict(lambda: collections.defaultdict(list))
-
 def findRefs(x, name=None):
     if isinstance(x, dict):
         if 'm_FileName' in x:
@@ -39,20 +36,38 @@ def findRefs(x, name=None):
         for v in x:
             yield from findRefs(v, name=name)
 
-print("Building references...")
-for path in tqdm(file_paths):
-    (folder_name, path_id) = re.match(r"(.*?)\/.*\#(\d+)\.json", path.replace("\\", "/")).groups()
-    source = FileID(folder_name, path_id)
-    with open(os.path.join(path), 'r', encoding="utf-8") as fp:
-        parsed = json.load(fp)
+def ddictlist():
+    # module level function, picklable
+    return collections.defaultdict(list)
 
-    # todo make this faster
-    for target, refd_as in findRefs(parsed):
-        referencesFrom[source].append(target)
-        referencedBy[target].append(source)
-        referencedAs[target][source].append(refd_as)
 
-@lru_cache(10000)
+referencesFrom = collections.defaultdict(list)
+referencedBy = collections.defaultdict(list)
+referencedAs = collections.defaultdict(ddictlist)
+
+reference_cache_filepath = "refs.pickle"
+try:
+    with open(reference_cache_filepath, "rb") as fp:
+        (referencesFrom, referencedBy, referencedAs) = pickle.load(fp)
+    print("Loaded cached references")
+except (FileNotFoundError, EOFError):
+    print("Building references...")
+    for path in tqdm(file_paths):
+        (folder_name, path_id) = re.match(r"(.*?)\/.*\#(\d+)\.json", path.replace("\\", "/")).groups()
+        source = FileID(folder_name, path_id)
+        with open(os.path.join(path), 'r', encoding="utf-8") as fp:
+            parsed = json.load(fp)
+
+        # todo make this faster
+        for target, refd_as in findRefs(parsed):
+            referencesFrom[source].append(target)
+            referencedBy[target].append(source)
+            referencedAs[target][source].append(refd_as)
+
+    with open(reference_cache_filepath, "wb") as fp:
+        tup = (referencesFrom, referencedBy, referencedAs,)
+        pickle.dump(tup, fp)
+
 def getReferencesHtml(file_id):
     if file_id not in referencedBy:
         return '<p>No references to this file</p>'
@@ -81,40 +96,63 @@ def fileIdToLink(ref):
         return f"<em>Unknown!</em> ({ref.fileName}/{ref.pathId})"
 
 
-def graphFileRefs(root):
+def graphFileRefs(root, max_dist=1):
 
     def fikey(file_id):
         return f"{str(file_id.fileName)}.{file_id.pathId}".replace(' ', '_').replace(')', '').replace('(', '')  
 
-    def _graphFileRefs(root, visited=[], by=True):
-        if by:
-            list_ = referencedBy
-        else:
-            list_ = referencesFrom
+    def _graphFileRefs(root, visited=[], mermaid_defined=[], recursive=True):
+        keys = []
 
-        for ref in list_[root]:
-            for refd_as in referencedAs[root][ref] + referencedAs[ref][root]:
-                key = (root, refd_as, ref)
-                if key in visited or len(visited) > 60:
-                    continue
-                if ref.fileName is None:
-                    continue
+        if root.fileName is None:
+            return
 
-                visited.append(key)
+        for source in referencedBy[root]:
+            for refd_as in referencedAs[root][source]:
+                # Root is referenced by name refd_as by source
+                key = (root, refd_as, source)
+                keys.append(key)
 
-                to_str = f"{fikey(ref)}[{fileIdToName(ref).replace(' ','_').replace(')', '').replace('(', '').replace('MonoBehaviour/', '') }]"
-                from_str = f"{fikey(root)}[{fileIdToName(root).replace(' ','_').replace(')', '').replace('(', '').replace('MonoBehaviour/', '') }]"
-                if by:
-                    yield f"  {to_str}-->|{refd_as}| {from_str}"
-                else:
-                    yield f"  {from_str}-->|{refd_as}| {to_str}"
+        for target in referencesFrom[root]:
+            for refd_as in referencedAs[target][root]:
+                # Target is referenced by name refd_as by root
+                key = (target, refd_as, root)
+                keys.append(key)
 
-                yield f"  click {fikey(ref)} \"{safe(f'/file/{fileIdToName(ref)}')}\""
-                yield f"  click {fikey(root)} \"{safe(f'/file/{fileIdToName(root)}')}\""
+        for key in keys:
+            (target, refd_as, source) = key
 
-                yield from _graphFileRefs(ref, visited, by)
+            if key in visited:
+                continue
 
-    ret = "graph TD\n" + "\n".join(list(_graphFileRefs(root, by=True))) + "\n" + "\n".join(list(_graphFileRefs(root, by=False)))
+            visited.append(key)
+
+            for node in [source, target]:
+                key = fikey(node)
+                if key not in mermaid_defined:
+                    po, pc = ("[", "]")
+
+                    # Mark root node w/ stadium
+                    if len(visited) == 1 and node is root:
+                        po, pc = ("([", "])")
+
+                    yield f"  {key}{po}{fileIdToName(node).replace(' ','_').replace(')', '').replace('(', '').replace('MonoBehaviour/', '')}{pc}"
+                    yield f"  click {key} \"{safe(f'/file/{fileIdToName(node)}')}\""
+
+                    if not recursive:
+                        yield f"  style {key} fill:#fff0"
+
+                    mermaid_defined.append(key)
+
+            to_str = f"{fikey(target)}"
+            from_str = f"{fikey(source)}"
+            yield f"  {from_str}-->|{refd_as}| {to_str}"
+
+            if recursive:
+                yield from _graphFileRefs(source, visited, mermaid_defined, recursive=False)
+                yield from _graphFileRefs(target, visited, mermaid_defined, recursive=False)
+
+    ret = "graph LR\n" + "\n".join(list(_graphFileRefs(root)))
     return ret
 
 
@@ -131,16 +169,17 @@ if __name__ == "__main__":
         )
         return f'<html><head></head><body>{ret}</body></html>', 200, {'Content-Type': 'text/html; charset=utf-8'}
 
-
     @app.route('/file/<archive>/MonoBehaviour/<filename>')
     def show(archive, filename):
         filename = filename.replace('$', '#')
+        print(filename)
         (path_id,) = re.match(r".*\#(\d+)\.json", filename).groups()
         with open(os.path.join(archive, 'MonoBehaviour', filename)) as f:
             parsed = json.load(f)
 
         fileId = FileID(archive, path_id)
         references = getReferencesHtml(fileId)
+        print("get", fileId)
 
         def traverse(x):
             if isinstance(x, dict):
@@ -159,12 +198,12 @@ if __name__ == "__main__":
                     traverse(v)
 
         traverse(parsed)
-        ret = '<script>mermaid.initialize({startOnLoad:true});</script>' + f'''<h1>{fileIdToLink(fileId)}</h1>
+        print("traversed")
+        ret = f'''<h1>{fileIdToLink(fileId)}</h1>
         {references}
-        <pre>{json.dumps(parsed, indent=4, sort_keys=False)}</pre>
-        <script src="https://cdn.jsdelivr.net/npm/mermaid/dist/mermaid.min.js"></script>
+        <pre>{json.dumps(parsed, indent=4, sort_keys=False)}</pre>''' + '<script src="https://cdn.jsdelivr.net/npm/mermaid/dist/mermaid.min.js"></script><script>mermaid.initialize({startOnLoad:true});</script>' + f'''        
         <div class='mermaid' style="overflow: auto;">{graphFileRefs(fileId)}</div>'''
+        print("done")
         return f'<html><head></head><body>{ret}</body></html>'
-
 
     app.run()
